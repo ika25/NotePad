c9e0dd3a0f6a1f4ab555d7cacf4c3c5f797b99c1
ika25 <lomiika1990@yahoo.com>
2020-07-01 16:57:59 +0100

    can create post and save in database

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,6 +1,37 @@
 const Post = require('../models/post');
+const slugify = require('slugify');
 
 
 exports.create = (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
+    const { title, content, user } = req.body;
+    const slug = slugify(title);
+    // validate (Going use switch statament more flexible)
+    /* if(!title || !content){
+        return res.status(400).json({
+            erro: 'Title and content is required'
+        });
+    }
+    res.json({
+        message: 'See your server console '
+    }); */
+    
+    // validate
+    switch (true) {
+        case !title:
+            return res.status(400).json({ error: 'Title is required' });
+            break;
+        case !content:
+            return res.status(400).json({ error: 'Content is required' });
+            break;
+    }
+    // create post
+    Post.create({ title, content, user, slug }, (err, post) => {
+        if (err) {
+            console.log(err);
+            res.status(400).json({ error: 'Duplicate post. Try another title' });
+        }
+        res.json(post);
+    });
+    
 };
